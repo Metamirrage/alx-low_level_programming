cdb9e4b0f8c46c4979e03ce12a00e89044d8e03b
Dan Kibaara <dankibaara99@gmail.com>
2022-06-04 11:01:42 -0700

    documentation style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-int main(void)/*This is the entry point for the programm*/
+/**
+ * This is the entry point for the programme*/
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle ");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
