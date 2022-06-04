b646c056511c6226638b64442870e8d0842c91ac
Dan Kibaara <dankibaara99@gmail.com>
2022-06-04 10:22:24 -0700

    main description

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-int main(void)
+int main(void)/*This is the entry point for the programm*/
 {
-	puts("\"Programming is like building a multilingual puzzle \n");
+	puts("\"Programming is like building a multilingual puzzle ");
 	return (0);
 }
